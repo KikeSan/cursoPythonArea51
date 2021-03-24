0dcd8b67d40c7a2876afdb42415e3a5da3473a9e
Kike Sanchez <kikin1601@hotmail.com>
2021-03-23 21:58:15 -0500

    Bucles

--- main.py
+++ main.py
@@ -1,53 +1,38 @@
-# CONDICIONALES & OPERADORES RACIONALES
 
-if 5 == 5:
-    print('es igual')
-
-elif 5 != 3:
-    print('Es diferente')
-
-elif 5 != 2:
-    print('Es diferente')
-
-elif 5 != 1:
-    print('Es diferente')
-
-else:
-    print('No cumple ninguna condicion')
-
-# El is es equivalente al === de js
-#if 1 is 1:
-#    print('Es correcto')
-
-lista = ['hola','mundo',1,2,3]
-if 'hola' in lista:
-    print('Si existe el elemento')
-
-if 'Python' not in lista:
-    print('No está en la lista')
-
-dictt = {
-    'name':'Kike'
-}
-if dictt.get('name'):
-    print('Hola Mundo')
-
-if dictt.get('last_name'):
-    print('Hola Mundo')
-
-if not dictt.get('last_name'):
-    print('Hola Mundo')
-
-# and or
-
-name = dictt.get('name')
-if not dictt.get('last_name') and name:
-    print(name)
-
-
-name = dictt.get('name')
-if dictt.get('last_name') or name:
-    print(name)
 
 # WHILE
+
+number = 0
+
+while number<=10:
+    print(f'El número en memoria es {number}')
+    number +=1
+
+print('Salió del bucle')
+
+##############################
+
+print('Ingrese 0 para salir del bucle')
+value = int(input('Ingrese elemento: '))
+
+while value!=0:
+    print('El elemento es diferente de 0')
+    print('Ingrese 0 para salir del bucle')
+
+    if value == 5:
+        print('Elemento detectado')
+        break  # rompe el loop
+
+    if value == 6:
+        print('Elemento detectado')
+        continue  # vuelve a ejecutar el bucle sin ejecutar el codigo siguiente
+
+    value = int(input('Ingrese elemento: '))
+else:
+    print('#'*50)
+    print('El elemento ingresado es 0')
+
+##############################
+
+
 
--- python2.py
+++ python2.py
@@ -90,3 +90,53 @@ dictt_4.pop('name')
 print(dictt_4)
 
 
+# CONDICIONALES & OPERADORES RACIONALES
+
+if 5 == 5:
+    print('es igual')
+
+elif 5 != 3:
+    print('Es diferente')
+
+elif 5 != 2:
+    print('Es diferente')
+
+elif 5 != 1:
+    print('Es diferente')
+
+else:
+    print('No cumple ninguna condicion')
+
+# El is es equivalente al === de js
+#if 1 is 1:
+#    print('Es correcto')
+
+lista = ['hola','mundo',1,2,3]
+if 'hola' in lista:
+    print('Si existe el elemento')
+
+if 'Python' not in lista:
+    print('No está en la lista')
+
+dictt = {
+    'name':'Kike'
+}
+if dictt.get('name'):
+    print('Hola Mundo')
+
+if dictt.get('last_name'):
+    print('Hola Mundo')
+
+if not dictt.get('last_name'):
+    print('Hola Mundo')
+
+# and or
+
+name = dictt.get('name')
+if not dictt.get('last_name') and name:
+    print(name)
+
+
+name = dictt.get('name')
+if dictt.get('last_name') or name:
+    print(name)
