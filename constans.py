f61488e1e3d65d09d28192af59d76b81fcb79ecf
Kike Sanchez <kikin1601@hotmail.com>
2021-03-23 20:12:30 -0500

    add gitignore

---
+++ constans.py
@@ -0,0 +1,1 @@
+URL_API_GOOGLE='http://google.com'
---
+++ python1.py
@@ -0,0 +1,93 @@
+value = 1
+print(type(value))
+
+value = 1.0
+print(type(value))
+
+value = True #-> 1/0
+print(type(value))
+
+
+#1.0
+print('hola '+'Mundo')
+
+#2.0-3.5
+print('Hola {} estoy aprendiendo'.format('Mundo','Python'))
+
+#3.6->
+text = 'Mundo'
+print(f'Hola {text}')
+
+
+# Constantes
+"""
+None
+True
+False
+
+if
+elif
+else
+
+and
+or
+
+for 
+while
+else
+
+try
+except
+
+pass
+continue
+
+del
+deflambda
+"""
+
+import constans
+
+print(constans.URL_API_GOOGLE)
+
+#operador de asignación
+r = 5
+print(r)
+
+#r = r+10
+r += 10
+print(r)
+r -= 10
+print(r)
+r *= 10
+print(r)
+r /= 10
+print(r)
+r %= 10
+print(r)
+
+value = 3+4
+print(value)
+
+value = int.__add__(7,4)
+print(value)
+
+#operadores racionales
+5 == 3
+5 != 3
+5 > 3
+5 >= 3
+5 <= 3
+
+#long = 42L
+
+value = 2
+print(type(value))
+
+value = float(2)
+print(type(value))
+
+value = str(2)
+print(type(value))
+
+value = 2.1 +7.8j
